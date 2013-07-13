2b012212250fca13b063da405a215a741e90d1a7
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-13 15:45:05 -0500

    d2: add jf selinux defconfig
    Change-Id: If9c5dc1a969605cd1eeb9218de02a9f8dbbd3ae1
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,7 +28,10 @@ BOARD_KERNEL_CMDLINE           := androidboot.hardware=qcom user_debug=31 zcache
 BOARD_KERNEL_BASE              := 0x80200000
 BOARD_MKBOOTIMG_ARGS           := --ramdisk_offset 0x01300000
 BOARD_KERNEL_PAGESIZE          := 2048
+TARGET_KERNEL_SELINUX_CONFIG   := m2selinux_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Recovery
 BOARD_USES_MMCUTILS := true
